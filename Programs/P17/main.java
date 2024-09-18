6c961c5943998dc6bcc082a82dfbb28b6f40a552
noirBreckin <112154255+noirBreckin@users.noreply.github.com>
2024-09-18 14:36:54 -0500

    Update main.java

--- Programs/P17/main.java
+++ Programs/P17/main.java
@@ -1,98 +1,129 @@
-import java.util.*;
+//import java.util.*;
 
- public class Main {
-  
-  public static class Frac{
+public class Main {
 
-      private int num;
-      private int den;
+  public static class Frac {
+
+    private int num;
+    private int den;
 
     ///////////////////////////////////////////////////////////////////
-    //default constructor
-    public Frac()
-    {
+    // default constructor
+    public Frac() {
       num = 1;
       den = 1;
     }
+
     ///////////////////////////////////////////////////////////////////
-    //non default constructor
-    public Frac(int n, int d)
-    {
+    // non default constructor
+    public Frac(int n, int d) {
       num = n;
       if (d == 0)
         System.out.println("Denominator cannot be 0");
       else
-      den = d;
+        den = d;
     }
+
     ///////////////////////////////////////////////////////////////////
-    //Setters
-    public void setNum(int n)
-    {
+    // Setters
+    public void setNum(int n) {
       num = n;
     }
+
     ///////////////////////////////////////////////////////////////////
-    public void setDen(int d)
-    {
+    public void setDen(int d) {
       den = d;
     }
+
     ///////////////////////////////////////////////////////////////////
-    //Getters
-    public int getNumerator()
-    {
-     return num;
+    // Getters
+    public int getNumerator() {
+      return num;
     }
+
     ///////////////////////////////////////////////////////////////////
-    public int getDenominator()
-    {
-    return den;
+    public int getDenominator() {
+      return den;
     }
+
     ///////////////////////////////////////////////////////////////////
-    //Math Operators
-    public  Frac mult(Frac f)
-    {
+    // Math Operators
+    public Frac mult(Frac f) {
       Frac temp = new Frac();
       temp.num = this.num * f.num;
       temp.den = this.den * f.den;
       return temp;
     }
+
     ///////////////////////////////////////////////////////////////////
-    public Frac add(Frac f, Frac g)
-    {
+    public Frac add(Frac f, Frac g) {
       Frac temp = new Frac();
       temp.num = (this.num * g.den) + (f.num * this.den);
       temp.den = this.den * g.den;
       return temp;
     }
+
     ///////////////////////////////////////////////////////////////////
-    public Frac sub(Frac f, Frac g)
-    {
+    public Frac sub(Frac f, Frac g) {
       Frac temp = new Frac();
       temp.num = (this.num * g.den) - (f.num * this.den);
       return temp;
     }
+
     ///////////////////////////////////////////////////////////////////
-    public Frac div(Frac f)
-    {
-    Frac temp = new Frac();
-    temp.num = this.num * f.den;
-    temp.den = this.den * f.num;
-    return temp;
+    public Frac div(Frac f) {
+      Frac temp = new Frac();
+      temp.num = this.num * f.den;
+      temp.den = this.den * f.num;
+      return temp;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // Reciprocal method
+    public Frac recip(Frac f) {
+      Frac temp = new Frac();
+      temp.num = f.den;
+      temp.den = f.num;
+      return temp;
     }
     ///////////////////////////////////////////////////////////////////
-    //Printer
-    public String toPrint()
-    {
+    public Frac gcd(Frac f){
+      Frac temp = new Frac();
+      int a = f.num;
+      int b = f.den;
+      int c = a % b;
+      while(c != 0){
+        a = b;
+        b = c;
+        c = a % b;
+      }
+      temp.num = b;
+      temp.den = 1;
+      return temp;
+    }
+    ///////////////////////////////////////////////////////////////////
+    public Frac reduce(Frac f){
+      Frac temp = new Frac();
+      Frac gcd = new Frac();
+      gcd = f.gcd(f);
+      temp.num = f.num / gcd.num;
+      temp.den = f.den / gcd.den;
+      return temp;
+    }
+    ///////////////////////////////////////////////////////////////////
+    // Printer
+    public String toPrint() {
       String s = this.num + "/" + this.den;
       return s;
     }
     ///////////////////////////////////////////////////////////////////
     ///////////////////////////////////////////////////////////////////
   }
-   //Main Method
-  public static void main(String[] args)
-  {
+
+  // Main Method
+  public static void main(String[] args) {
     Frac f1 = new Frac();
-    Frac f2 = new Frac(1,2);
+    Frac f2 = new Frac(1, 2);
     Frac f3;
     Frac f4;
     Frac f5;
@@ -116,7 +147,12 @@ import java.util.*;
     f6 = f1.div(f2);
     s1 = f6.toPrint();
     System.out.println(s1);
+    f6 = f1.recip(f2);
+    s1 = f6.toPrint();
+    System.out.println(s1);
+    f6 = f1.reduce(f2);
+    s1 = f6.toPrint();
+    System.out.println(s1);
   }
 
-  
 }
