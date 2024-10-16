c7e6ace5261b98f5703f1ac4ea1a701cc55ca912
noirBreckin <112154255+noirBreckin@users.noreply.github.com>
2024-10-16 12:45:58 -0500

    Update main.py

--- Programs/P18/main.py
+++ Programs/P18/main.py
@@ -3,15 +3,53 @@
 #10/2/2024
 #Python Intro
 
+
+def recursive_sum(n):
+  if n == 1:
+    return 1
+  else:
+    return n + recursive_sum(n - 1)
+
+
+def classify(x):
+  a = input("For or while loop? Type F for for loop or W for while loop: ")
+  if a == "F":
+    sum = 0
+    for i in range(1, x):
+      if x % i == 0:
+        sum += i
+    if sum == x:
+      return "p"
+    elif sum < x:
+      return "d"
+    elif sum > x:
+      return "a"
+  if a == "W":
+    sum = 0
+    i = 1
+    while i < x:
+      if x % i == 0:
+        sum += i
+    if sum == x:
+      return "p"
+    elif sum < x:
+      return "d"
+    elif sum > x:
+      return "a"
+  
+
 print("Hello there, lets learn python :)")
 
-num = int(input("Enter a number: "))
+num = int(input("Enter a number to have sum calculated: "))
 sum = 0
 for i in range(0, num + 1):
   sum += i
 print(sum)
 
-num2 = int(input("Enter another number: "))
+num5 = int(input("Please enter a number to have sum calculated recursively: "))
+print(recursive_sum(num5))
+
+num2 = int(input("Enter another number to be categorized as odd, even etc: "))
 if num2 % 2 == 0:
   print("Your number is even")
 elif num2 % 2 != 0:
@@ -23,7 +61,7 @@ elif num2 < 0:
 if num2 == 0:
   print("Your number is zero")
 
-num3 = int(input("Enter another number: "))
+num3 = int(input("Enter another number for calculating squares: "))
 for i in range(0, num3 + 1):
   print("Current number is " + str(i) + " and the square is " + str(i * i))
 
@@ -41,3 +79,14 @@ slicesPerPerson = slices // people
 leftover = slices % people
 print("Each person will get " + str(slicesPerPerson) + " and there will be " +
       str(leftover) + " slices leftover")
+
+num4 = int(input("Please enter a number to be classified: "))
+d = classify(num4)
+if d == "p":
+  print("Your number is perfect!")
+elif d == "d":
+  print("Your number is deficient!")
+elif d == "a":
+  print("Your number is abundant!")
+
+
